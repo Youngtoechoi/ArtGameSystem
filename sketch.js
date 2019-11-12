15ed8f9c238d0e8a30da763e59c9dfd29becd60b
Youngtoechoi <56008125+Youngtoechoi@users.noreply.github.com>
2019-11-12 11:22:49 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,20 @@
-//Creating animations
+//Controlling animations
+//click, press and hold to see different behaviors
+var circle, explode, sleep, glitch;
 
-//animations like p5 images should be stored in variables
-//in order to be displayed during the draw cycle
-var ghost, asterisk;
-
-//it's advisable (but not necessary) to load the images in the preload function
-//of your sketch otherwise they may appear with a little delay
 function preload() {
 
-  //create an animation from a sequence of numbered images
-  //pass the first and the last file name and it will try to find the ones in between
-  ghost = loadAnimation('assets/ghost_standing0001.png', 'assets/ghost_standing0007.png');
+  sleep = loadAnimation('assets/asterisk_stretching0001.png', 'assets/asterisk_stretching0008.png');
 
-  //create an animation listing all the images files
-  asterisk = loadAnimation('assets/asterisk.png', 'assets/triangle.png', 'assets/square.png', 'assets/cloud.png', 'assets/star.png', 'assets/mess.png', 'assets/monster.png');
+  circle = loadAnimation('assets/asterisk_circle0000.png', 'assets/asterisk_circle0008.png');
+  //by default animations loop but it can be changed
+  circle.looping = false;
+
+  explode = loadAnimation('assets/asterisk_explode0001.png', 'assets/asterisk_explode0011.png');
+
+  glitch = loadAnimation('assets/asterisk.png', 'assets/triangle.png', 'assets/square.png', 'assets/cloud.png', 'assets/star.png', 'assets/mess.png', 'assets/monster.png');
+  //by default an animation plays but you may not want that
+  glitch.playing = false;
 }
 
 function setup() {
@@ -21,10 +22,37 @@ function setup() {
 }
 
 function draw() {
-  background(100, 100, 100);
+  background(255, 255, 255);
 
-  //specify the animation instance and its x,y position
-  //animation() will update the animation frame as well
-  animation(ghost, 300, 150);
-  animation(asterisk, 500, 150);
+  //playing an pausing an animation
+  if(mouseIsPressed)
+    sleep.play();
+  else
+    sleep.stop();
+
+  //reading and changing the current frame
+  if(explode.getFrame()==explode.getLastFrame())
+    explode.changeFrame(7);
+
+  //playing backward or forward toward a specific frame
+  //returns to the initial frame if click and hold
+  if(mouseIsPressed)
+    circle.goToFrame(0);
+  else
+    circle.goToFrame(circle.getLastFrame());
+
+  animation(sleep, 100, 150);
+  animation(explode, 300, 150);
+  animation(glitch, 500, 150);
+  animation(circle, 700, 150);
+
+}
+
+function mousePressed() {
+  //rewind on mouse pressed - change frame to 0
+  explode.rewind();
+
+  //move ahead one frame
+  glitch.nextFrame();
+  //glitch.previousFrame();
 }
