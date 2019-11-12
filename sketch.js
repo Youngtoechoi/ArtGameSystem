34cefcdfb4f3eb38fe49aa4aeca35fa655977ae8
Youngtoechoi <56008125+Youngtoechoi@users.noreply.github.com>
2019-11-12 11:45:49 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,9 +2,13 @@
 //Click to create a new sprite with random speed
 var s;
 var wall;
+var canvas;
+let canvasWidth = 600;
+let canvasHeight = 400;
 
 function setup() {
-  createCanvas(800, 400);
+  canvas =createCanvas(canvasWidth, canvasHeight);
+  canvas.position(windowWidth/2 - canvasWidth/2, 20);
 }
 
 function draw() {
