5c9f4132705ce5590473e48a9ca91bb2c3596d68
Youngtoechoi <56008125+Youngtoechoi@users.noreply.github.com>
2019-11-17 02:27:45 +0900

    make 's' move with keyboard
    made 's' move with keyboard Arrows

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let canvas;
 let canvasWidth = 600;
 let canvasHeight = 400;
+let sX,sY =0, height/2;
 
 
 function setup() {
@@ -12,10 +13,11 @@ function setup() {
 function draw() {
   background(255,255,255)
 
-  var s = createSprite(0, height/2, 30, 30)
+  var s = createSprite(sX, sY, 30, 30)
   var wall = createSprite(width/2, height/2, 10, 10)
 
   if (s.overlap(wall)){
+
     wall.visible = false;
   }
   else {
@@ -23,3 +25,14 @@ function draw() {
   }
   drawSprites();
 }
+keyPressed(){
+  if (keyCode == LEFT_ARROW){
+  sX=  sX-5}
+  else if (keyCode == RIGHT_ARROW){
+  sX=  sX+5}
+  else if (keyCode == DOWN_ARROW){
+  sY=  sY+5}
+  else if (keyCode == UP_ARROW){
+  sY=  sY-5}
+
+  }
