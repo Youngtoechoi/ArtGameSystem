5e65507ce46cc7b7bc0eb4f3e66d6e94fbc58554
Youngtoechoi <56008125+Youngtoechoi@users.noreply.github.com>
2019-11-17 01:47:54 +0900

    draw ellipse
    draing ellipse

--- sketch.js
+++ sketch.js
@@ -4,16 +4,10 @@ let zoom
 
 function setup(){
   createCanveas(800,400)
-
-  ghost = createSprite(400, 200, 50, 100);
-
-  var myAnimation = ghost.addAnimation('floating', 'assets/ghost_standing0001.png', 'assets/ghost_standing0007.png')
-  myAnimation.offY = 18;
-  ghost.addAnimation('moving', 'assets/ghost_walk0001.png', 'assets/ghost_walk0004.png');
+ ellipse(width/2, height/2, 30)
 
 }
 
 function draw() {
 
-drawSprites(ghost);
 }
