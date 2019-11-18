d1e0d6c20f814f43b509633ce683053aeb83d183
Youngtoechoi <56008125+Youngtoechoi@users.noreply.github.com>
2019-11-18 16:06:37 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,36 @@
 let canvas;
 let canvasWidth = 600;
 let canvasHeight = 400;
+let sX =0;
+let sY =200;
+var s, wall;
 
 function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
   noCursor();
+
+   s = createSprite(sX, sY, 30, 30)
+   wall = createSprite(width/2, height/2, 10, 10)
+
 }
 
 function draw() {
-  background(30);
-  rect(mouseX, mouseY, 100, 100);
+  background(255,255,255)
+  s.position.x = sX;
+  s.position.y = sY;
+
+  s.collide(wall);
+  drawSprites();
 }
+function keyPressed(){
+  if (keyCode == LEFT_ARROW){
+  sX=  sX-5}
+  else if (keyCode == RIGHT_ARROW){
+  sX=  sX+5}
+  else if (keyCode == DOWN_ARROW){
+  sY=  sY+5}
+  else if (keyCode == UP_ARROW){
+  sY=  sY-5}
+
+  }
