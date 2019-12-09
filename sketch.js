1e4197c9a00b8405829b9a37a72432c526c143f8
Youngtoechoi <56008125+Youngtoechoi@users.noreply.github.com>
2019-12-10 08:03:34 +0900

    to create passenger
    created random passengers and changed description

--- sketch.js
+++ sketch.js
@@ -1,14 +1,26 @@
 let canvas;
 let canvasWidth = 600;
 let canvasHeight = 400;
+var meX, meY;
+var me;
+var passengers;
 
 function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
   noCursor();
+  passengers = new Group();
+  for(var i=0; i<5; i++)
+  {
+    var passenger = createSprite(random(0,width), random(0, height),30,30);
+    passenger.setSpeed(random(2,3),random (0,360));
+    passengers.add(passenger);
+  }
 }
 
 function draw() {
   background(30);
-  rect(mouseX, mouseY, 100, 100);
+
+
+drawSprites();
 }
